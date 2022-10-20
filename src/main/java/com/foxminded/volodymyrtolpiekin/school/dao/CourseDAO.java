1cdab3fff9e6c1a2832f30fe55cf761a3a3242d8
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2022-10-20 14:56:46 +0300

    Correction.

--- src/main/java/com/foxminded/volodymyrtolpiekin/school/dao/CourseDAO.java
+++ src/main/java/com/foxminded/volodymyrtolpiekin/school/dao/CourseDAO.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class CourseDAO implements DAO<Course>{
+public class CourseDAO implements DAO<Course>{
 
     private List<Course> courses = new ArrayList<>();
     @Override
